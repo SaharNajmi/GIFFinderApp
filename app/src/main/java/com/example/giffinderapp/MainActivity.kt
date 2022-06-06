b5b9153e23cab0eedae9c7b4a8b2feb0bc848b9b
sahar <7899798-saharnajmi@users.noreply.gitlab.com>
2022-06-06 16:57:29 +0330

    UI Design (MainLayout and SearchLayout)

--- app/src/main/java/com/example/giffinderapp/MainActivity.kt
+++ app/src/main/java/com/example/giffinderapp/MainActivity.kt
@@ -1,40 +1,257 @@
 package com.example.giffinderapp
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsPressedAsState
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.giffinderapp.ui.theme.GIFFinderAppTheme
 
 class MainActivity : ComponentActivity() {
+    lateinit var stateVisibilityLayout: MutableState<Boolean>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             GIFFinderAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                stateVisibilityLayout = remember { mutableStateOf(true) }
+                Surface(
+                    modifier = Modifier
+                        .fillMaxSize(),
+                    color = Color(0xFF0D1017)
+                ) {
+
+                    Column(modifier = Modifier.padding(15.dp)) {
+                        if (stateVisibilityLayout.value)
+                            MainLayout()
+                        else
+                            SearchLayout()
+                    }
                 }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+    @Composable
+    fun SearchLayout() {
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            BackImage()
+            SearchView()
+        }
+        ShowMessageText("Result")
+        GifList(gifList = listOf("", "", "", "", "", ""))
+    }
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    GIFFinderAppTheme {
-        Greeting("Android")
+    @Composable
+    fun MainLayout() {
+        SearchBar()
+        ShowMessageText("Random gif")
+        CardGif()
+    }
+
+
+    @Composable
+    private fun GifList(gifList: List<String>) {
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(3),
+            contentPadding = PaddingValues(10.dp),
+        ) {
+            items(gifList.size) { index ->
+                GifItem()
+            }
+        }
+    }
+
+    @Composable
+    fun BackImage() {
+        Image(
+            painter = painterResource(id = R.drawable.ic_back),
+            contentDescription = null,
+            modifier = Modifier
+                .padding(end = 5.dp)
+                .clickable {
+                    stateVisibilityLayout.value = true
+                }
+        )
+    }
+
+    @Composable
+    fun ShowMessageText(text: String) {
+        Text(
+            text = text,
+            color = Color.White,
+            modifier = Modifier.padding(top = 18.dp, bottom = 18.dp)
+        )
+    }
+
+    @Composable
+    fun SearchBar() {
+        val source = remember { MutableInteractionSource() }
+
+        if (source.collectIsPressedAsState().value) {
+            stateVisibilityLayout.value = false
+        }
+
+        TextField(
+            value = "Search",
+            onValueChange = {
+            },
+            modifier = Modifier
+                .fillMaxWidth(),
+            textStyle = TextStyle(color = Color.White, fontSize = 18.sp),
+            leadingIcon = {
+                Icon(
+                    Icons.Default.Search,
+                    contentDescription = "",
+                    modifier = Modifier
+                        .padding(15.dp)
+                        .size(24.dp)
+                )
+            },
+            singleLine = true,
+            readOnly = true,
+            interactionSource = source,
+            shape = RoundedCornerShape(15.dp),
+            colors = TextFieldDefaults.textFieldColors(
+                textColor = Color.White,
+                cursorColor = Color.White,
+                leadingIconColor = Color.White,
+                trailingIconColor = Color.White,
+                backgroundColor = colorResource(id = R.color.lightBlack),
+                placeholderColor = Color.White,
+                focusedIndicatorColor = Color.Transparent
+            )
+        )
+    }
+
+    @Composable
+    fun SearchView() {
+        val textState = remember { mutableStateOf(TextFieldValue("")) }
+        TextField(
+            value = textState.value,
+            onValueChange = { value ->
+                textState.value = value
+            },
+            modifier = Modifier
+                .fillMaxWidth(),
+            placeholder = { Text("Search") },
+            textStyle = TextStyle(color = Color.White, fontSize = 18.sp),
+            leadingIcon = {
+                Icon(
+                    Icons.Default.Search,
+                    contentDescription = "",
+                    modifier = Modifier
+                        .padding(15.dp)
+                        .size(24.dp)
+                )
+            },
+            trailingIcon = {
+                if (textState.value != TextFieldValue("")) {
+                    IconButton(
+                        onClick = {
+                            textState.value =
+                                TextFieldValue("") // Remove text from TextField
+                        }
+                    ) {
+                        Icon(
+                            Icons.Default.Close,
+                            contentDescription = "",
+                            modifier = Modifier
+                                .padding(15.dp)
+                                .size(24.dp)
+                        )
+                    }
+                }
+            },
+            singleLine = true,
+            shape = RoundedCornerShape(15.dp),
+            colors = TextFieldDefaults.textFieldColors(
+                textColor = Color.White,
+                cursorColor = Color.White,
+                leadingIconColor = Color.White,
+                trailingIconColor = Color.White,
+                backgroundColor = colorResource(id = R.color.lightBlack),
+                placeholderColor = Color.White,
+                focusedIndicatorColor = Color.Transparent
+            )
+        )
+    }
+
+    @Composable
+    fun CardGif() {
+        Card(
+            modifier = Modifier,
+            shape = RoundedCornerShape(15.dp),
+            elevation = 2.dp
+        )
+        {
+            Image(
+                painter = painterResource(id = R.drawable.ic_gif),
+                contentDescription = null,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(380.dp)
+                    .background(Color(0xFF161B21))
+            )
+        }
+    }
+
+    @Composable
+    fun GifItem() {
+        Card(
+            modifier = Modifier.padding(end = 5.dp, start = 5.dp, bottom = 8.dp),
+            shape = RoundedCornerShape(10.dp),
+            elevation = 2.dp
+        )
+        {
+            Image(
+                painter = painterResource(id = R.drawable.ic_launcher_background),
+                contentDescription = null,
+                contentScale = ContentScale.FillBounds,
+                modifier = Modifier
+                    .width(150.dp)
+                    .height(150.dp)
+                    .background(Color(0xFF161B21))
+                    .clickable {
+                        Toast
+                            .makeText(this, "item Click", Toast.LENGTH_SHORT)
+                            .show()
+                    }
+            )
+        }
+    }
+
+    @Preview(showBackground = true)
+    @Composable
+    fun DefaultPreview() {
+        GIFFinderAppTheme {
+            MainLayout()
+        }
     }
 }
